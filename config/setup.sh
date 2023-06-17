38fb7abd3435d18bbd0b97714c0e04c2d48edf1d
0xSixty6 <117480800+0xSixty6@users.noreply.github.com>
2023-06-17 16:52:49 +0700

    Update setup.sh

--- config/setup.sh
+++ config/setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+echo "------ install ansible -------------"
 # install ansible
 sudo apt install ansible -y
 
 ansible-playbook main.yml -i inventory.ini --ask-become-pass
 
+echo "--------- install neovim --------------"
 # install neovim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
@@ -14,6 +16,7 @@ chmod u+x nvim.appimage
 sudo mv squashfs-root /
 sudo ln -sf /squashfs-root/AppRun /usr/bin/nvim
 
+echo "------ install nvim ----------"
 # install nvim
 if [ -d $HOME'/.config/nvim' ]; then
   mv ~/.config/nvim ~/.config/nvim.backup
@@ -24,20 +27,24 @@ else
   git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
 fi
 
+echo "------ Install font ------------"
 # install font
-mkdir ~/.local/share/fonts/
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Hack.zip
+mkdir $HOME/.local/share/fonts/Hack.zip
+wget -o $HOME/.local/share/fonts/ https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Hack.zip
 unzip Hack.zip
 rm Hack.zip
 fc-cache -fv
 
+echo "--------- oh-my-zsh ----------"
 # install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Powerlevel10k 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 rm $HOME/.zshrc
 wget -O $HOME/.zshrc https://github.com/0xSixty6/setup-env/blob/main/oh-my-zsh/.zshrc
+rm $HOME/.zshrc
+mv $HOME/.zshrc.1 $HOME/.zshrc
 exec zsh
 
 vim
