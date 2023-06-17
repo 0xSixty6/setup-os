9e3a69dc23c0f2373a54900409615f9754195edb
0xSixty6 <117480800+0xSixty6@users.noreply.github.com>
2023-06-17 17:27:52 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -47,7 +47,7 @@ sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools
 
 # Powerlevel10k 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-wget -O $HOME/.zshrc https://github.com/0xSixty6/setup-env/blob/main/oh-my-zsh/.zshrc
+wget -O $HOME/.zshrc https://raw.githubusercontent.com/0xSixty6/setup-env/main/oh-my-zsh/.zshrc
 rm $HOME/.zshrc
 mv $HOME/.zshrc.1 $HOME/.zshrc
 exec zsh
